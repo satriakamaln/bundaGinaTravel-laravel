642776276fa14a74bab9e535f75bc934145809d2
dedovis <dedovis@gmail.com>
2023-02-19 18:10:20 +0800

    middleware

--- routes/web.php
+++ routes/web.php
@@ -25,15 +25,18 @@ Route::get('/', function () {
 });
 Auth::routes();
 
-Route::prefix('admin')->name('admin.')->group(function () {
-    Route::get('/home', [HomeController::class, 'index'])->name('home');
+Route::middleware(['admin'])->group(function() {
 
-    Route::resource('user', UserController::class);
-    Route::resource('mobil', MobilController::class);
-    Route::resource('travel', TravelController::class);
-    Route::resource('wisata', WisataController::class);
 
-});
+    Route::prefix('admin')->name('admin.')->group(function () {
+        Route::get('home', [HomeController::class, 'index'])->name('home');
+
+        Route::resource('user', UserController::class);
+        Route::resource('mobil', MobilController::class);
+        Route::resource('travel', TravelController::class);
+        Route::resource('wisata', WisataController::class);
+
+    });
         Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
         // Route::get('/travelindex', [App\Http\Controllers\TravelController::class, 'index'])->name('travel');
@@ -54,3 +57,4 @@ Route::prefix('admin')->name('admin.')->group(function () {
         Route::get('/pemesanan', [App\Http\Controllers\PemesananController::class, 'index'])->name('pemesanan');
 
         Route::get('/pembayaran', [App\Http\Controllers\PembayaranController::class, 'index'])->name('pembayaran');
+});
