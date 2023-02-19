6ee170964618695c880bbf25532202837a4a60d3
dedovis <dedovis@gmail.com>
2023-02-19 23:18:58 +0800

    rental

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -83,7 +83,7 @@
                             <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">Pelayanan</a>
                             <div class="dropdown-menu border-0 rounded-0 m-0">
                                 <a href="{{ route('wisata') }}" class="dropdown-item">Wisata</a>
-                                <a href="single.html" class="dropdown-item">Rental Mobil</a>
+                                <a href="{{ route('rental') }}" class="dropdown-item">Rental Mobil</a>
                                 <a href="{{ route('travel') }}" class="dropdown-item">Travel</a>
                             </div>
                         </div>
