9f2875bf2f66f8c6293dfa12f7816d1d830f3f35
dedovis <dedovis@gmail.com>
2023-02-24 06:04:36 +0800

    fix wisata show

--- resources/views/customer/wisata/show.blade.php
+++ resources/views/customer/wisata/show.blade.php
@@ -18,8 +18,8 @@ Wisata
                 <div class="col-lg-6 pt-5 pb-lg-5">
                     <div class="about-text bg-white p-4 p-lg-5 my-lg-5">
                         <h6 class="text-primary text-uppercase" style="letter-spacing: 5px;">{{ $wisata->nama_wisata }}</h6>
-                        <h1 class="mb-3">We Provide Best Tour Packages In Your Budget</h1>
-                        <p>Dolores lorem lorem ipsum sit et ipsum. Sadip sea amet diam dolore sed et. Sit rebum labore sit sit ut vero no sit. Et elitr stet dolor sed sit et sed ipsum et kasd ut. Erat duo eos et erat sed diam duo</p>
+                        <h1 class="mb-3">{{ $wisata->lokasi }}</h1>
+                        <p>{{ $wisata->deskripsi }}</p>
                         {{-- <div class="row mb-4">
                             <div class="col-6">
                                 <img class="img-fluid" src="{{ asset('front/img/about-1.jpg') }}" alt="">
@@ -28,7 +28,7 @@ Wisata
                                 <img class="img-fluid" src="{{ asset('front/img/about-2.jpg') }}" alt="">
                             </div>
                         </div> --}}
-                        <a href="" class="btn btn-primary mt-1">Book Now</a>
+                        {{-- <a href="" class="btn btn-primary mt-1">Book Now</a> --}}
                     </div>
                 </div>
             </div>
